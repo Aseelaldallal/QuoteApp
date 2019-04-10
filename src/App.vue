c6552fff98b78c3c595ab990ecb89b78b7b14e4d
Aseel Al Dallal <asoola@Aseels-MacBook-Air.local>
2019-04-10 19:40:26 -0400

    backbone

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="container">
-        
-    
+        <app-quotes-adder @quoteAdded="addQuote($event)"></app-quotes-adder>
+        <div> 
+            <app-quote v-for="(quote,i) in quotes" :key="i">{{quote}}</app-quote>
+        </div>
     </div>
 </template>
 
 <script>
+    import QuotesAdder from './components/QuotesAdder.vue';
+    import Quote from './components/Quote.vue';
     export default {
+        data() {
+            return {
+                quotes:[]
+            }
+        }, 
+        methods: {
+            addQuote(quote) {
+                console.log('Adding quote', quote);
+                this.quotes.push(quote);
+            }
+        },
+        components: {
+            'app-quotes-adder': QuotesAdder,
+            'app-quote': Quote
+        },
         
     }
 </script>
---
+++ src/components/QuotesAdder.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h5>Quote</h5>
+        <textarea cols="30" rows="10" v-model="quote"></textarea>
+        <button @click="addQuote" class="btn btn-primary"> Add Quote </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quote: 'Quack'
+            }
+        }, 
+        methods: {
+            addQuote() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
