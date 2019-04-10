027d875703a0ef519d7def5fcc50ed1d050b221a
Aseel Al Dallal <asoola@Aseels-MacBook-Air.local>
2019-04-10 19:18:04 -0400

    tracker backbone

---
+++ src/components/QuoteTracker.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Quotes Added</h1>
+        <div class="progressBar">
+            <div class="progress"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
